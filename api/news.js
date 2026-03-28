a60e777a2f876fa07e961b6a2d728123a1c122c3
alae-G5 <alaedine309@gmail.com>
2026-03-28 17:56:53 +0100

    Add config export for Node.js runtime

--- api/news.js
+++ api/news.js
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: "nodejs"
+};
+
 export default async function handler(req, res) {
   const topic = req.query.topic || "technology";
 
@@ -43,3 +47,4 @@ export default async function handler(req, res) {
     res.status(500).json({ articles: [], error: e.toString() });
   }
 }
+
